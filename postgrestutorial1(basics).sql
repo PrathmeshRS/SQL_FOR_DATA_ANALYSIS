bb9e4e4edf1f89750245d789f46f91f7a9b4e84d
prathmesh shinde <shindeprathmesh99@gmail.com>
2022-12-31 22:03:06 +0530

    End Of 2022. Happy New Year 2023!

---
+++ postgrestutorial1(basics).sql
@@ -0,0 +1,248 @@
+-- LIMIT
+
+-- Try using LIMIT yourself below by writing a query that displays all the data in the occurred_at, account_id, and channel columns of the web_events table, and limits the output to only the first 15 rows.
+SELECT occurred_at, account_id, channel 
+FROM web_events 
+LIMIT 15;
+
+-------------------------------------------------------------------------------
+-- ORDER BY
+
+-- Write a query to return the 10 earliest orders in the orders table. Include the id, occurred_at, and total_amt_usd.
+SELECT id, occurred_at, total_amt_usd
+FROM orders
+ORDER BY occurred_at
+LIMIT 10;
+-- The ORDER BY statement always comes in a query after the SELECT and FROM statements, but before the LIMIT statement. If you are using the LIMIT statement, it will always appear last. As you learn additional commands, the order of these statements will matter more.
+
+-- Write a query to return the top 5 orders in terms of largest total_amt_usd. Include the id, account_id, and total_amt_usd.
+SELECT id, account_id, total_amt_usd
+FROM orders
+ORDER BY total_amt_usd
+LIMIT 5;
+
+-- Write a query to return the lowest 20 orders in terms of smallest total_amt_usd. Include the id, account_id, and total_amt_usd.
+SELECT id, account_id, total_amt_usd
+FROM orders
+ORDER BY total_amt_usd DESC
+LIMIT 20;
+
+-- Write a query that displays the order ID, account ID, and total dollar amount for all the orders, sorted first by the account ID (in ascending order), and then by the total dollar amount (in descending order).
+SELECT id, account_id, total_amt_usd
+FROM orders
+ORDER BY account_id ASC, total_amt_usd DESC;
+
+-- Now write a query that again displays order ID, account ID, and total dollar amount for each order, but this time sorted first by total dollar amount (in descending order), and then by account ID (in ascending order).
+SELECT id, account_id, total_amt_usd
+FROM orders
+ORDER BY total_amt_usd DESC, account_id ASC;
+
+------------------------------------------------------------------------------------
+-- WHERE
+/*Using the WHERE statement, we can display subsets of tables based on conditions that must be met. You can also think of the WHERE command as filtering the data.
+
+This video above shows how this can be used, and in the upcoming concepts, you will learn some common operators that are useful with the WHERE' statement.
+
+Common symbols used in WHERE statements include:
+
+> (greater than)
+
+< (less than)
+
+>= (greater than or equal to)
+
+<= (less than or equal to)
+
+= (equal to)
+
+!= (not equal to)
+*/
+-----------------------------------------------------------------------------------
+-- Pull the first 5 rows and all columns from the orders table that have a dollar amount of gloss_amt_usd greater than or equal to 1000.
+SELECT *
+FROM orders
+WHERE gloss_amt_usd >= 1000;
+
+-- Pull the first 10 rows and all columns from the orders table that have a total_amt_usd less than 500.
+SELECT *
+FROM orders
+WHERE total_amt_usd < 500
+LIMIT 10;
+
+/*
+--WHERE WITH NON-NUMERIC
+The WHERE statement can also be used with non-numeric data. We can use the = and != operators here. You need to be sure to use single quotes (just be careful if you have quotes in the original text) with the text data, not double quotes.
+
+Commonly when we are using WHERE with non-numeric data fields, we use the LIKE, NOT, or IN operators.
+ 
+*/
+-- Filter the accounts table to include the company name, website, and the primary point of contact (primary_poc) just for the Exxon Mobil company in the accounts table.
+SELECT name, website, primary_poc
+FROM accounts
+WHERE name='Exxon Mobil';
+
+-----------------------------------------------------------------------------
+-- ARITHMETIC OPERATORS
+/*
+Creating a new column that is a combination of existing columns is known as a derived column (or "calculated" or "computed" column). Usually you want to give a name, or "alias," to your new column using the AS keyword.
+
+This derived column, and its alias, are generally only temporary, existing just for the duration of your query. The next time you run a query and access this table, the new column will not be there.
+
+If you are deriving the new column from existing columns using a mathematical expression, then these familiar mathematical operators will be useful:
+
+* (Multiplication)
++ (Addition)
+- (Subtraction)
+/ (Division)
+*/
+-- Using orders table Create a column that divides the standard_amt_usd by the standard_qty to find the unit price for standard paper for each order. Limit the results to the first 10 orders, and include the id and account_id fields.
+SELECT id, account_id, standard_amt_usd/standard_qty AS price
+FROM orders
+LIMIT 10;
+
+-- Write a query that finds the percentage of revenue that comes from poster paper for each order. You will need to use only the columns that end with _usd. (Try to do this without using the total column.) Display the id and account_id fields also.
+SELECT id, account_id, (poster_amt_usd/total_amt_usd)*100 AS poster_revenue_percentage
+FROM orders
+LIMIT 10;
+
+SELECT id, account_id, 
+   poster_amt_usd/(standard_amt_usd + gloss_amt_usd + poster_amt_usd) AS post_per
+FROM orders
+LIMIT 10;
+
+---------------------------------------------------------------------------------
+-- LOGICAL OPERATORS
+/*
+1. LIKE -This allows you to perform operations similar to using WHERE and =, but for cases when you might not know exactly what you are looking for.
+2. IN -This allows you to perform operations similar to using WHERE and =, but for more than one condition.
+3. NOT -This is used with IN and LIKE to select all of the rows NOT LIKE or NOT IN a certain condition.
+4. AND & BETWEEN -These allow you to combine operations where all combined conditions must be true.
+5. OR -This allows you to combine operations where at least one of the combined conditions must be true.
+*/
+
+/*
+LIKE OPERATOR
+The LIKE operator is extremely useful for working with text. You will use LIKE within a WHERE clause. The LIKE operator is frequently used with %. The % tells us that we might want any number of characters leading up to a particular set of characters or following a certain set of characters, as we saw with the google syntax above. Remember you will need to use single quotes for the text you pass to the LIKE operator, because of this lower and uppercase letters are not the same within the string. Searching for 'T' is not the same as searching for 't'. In other SQL environments (outside the classroom), you can use either single or double quotes.
+*/
+
+-- Use the accounts table to find all the companies whose names start with 'C'.
+SELECT name
+FROM accounts
+WHERE name LIKE 'C%';
+
+-- Use the accounts table to find all companies whose names contain the string 'one' somewhere in the name.
+SELECT name
+FROM accounts
+WHERE name LIKE '%one%';
+
+-- Use the accounts table to find all companies whose names end with 's'.
+SELECT name
+FROM accounts
+WHERE name LIKE '%s';
+
+/*
+IN OPERATOR
+The IN operator is useful for working with both numeric and text columns. This operator allows you to use an =, but for more than one item of that particular column. 
+We can check one, two or many column values for which we want to pull data, but all within the same query. 
+The OR operator that also allows us to perform these tasks, but the IN operator is a cleaner way to write these queries.
+*/
+
+-- Use the accounts table to find the account name, primary_poc, and sales_rep_id for Walmart, Target, and Nordstrom.
+SELECT name, primary_poc, sales_rep_id
+FROM accounts
+WHERE name IN ('Walmart', 'Target', 'Nordstrom');
+
+-- Use the web_events table to find all information regarding individuals who were contacted via the channel of organic or adwords.
+SELECT *
+FROM web_events
+WHERE channel IN ('organic', 'adwords');
+
+/*
+NOT OPERATOR
+The NOT operator is an extremely useful operator for working with the previous two operators we introduced: IN and LIKE. 
+By specifying NOT LIKE or NOT IN, we can grab all of the rows that do not meet a particular criteria.
+*/
+
+-- Use the accounts table to find the account name, primary poc, and sales rep id for all stores except Walmart, Target, and Nordstrom.
+SELECT name, primary_poc, sales_rep_id
+FROM accounts
+WHERE name NOT IN ('Walmart', 'Target', 'Nordstrom');
+
+-- Use the web_events table to find all information regarding individuals who were contacted via any method except using organic or adwords methods.
+SELECT *
+FROM web_events
+WHERE channel NOT IN ('organic', 'adwords');
+
+-- Use accounts table to find all the companies whose names do not start with 'C'.
+SELECT *
+FROM accounts
+WHERE name NOT LIKE 'C%';
+
+-- Use accounts table to find all companies whose names do not contain the string 'one' somewhere in the name.
+SELECT *
+FROM accounts
+WHERE name NOT LIKE '%one%';
+
+-- Use accounts table to find all companies whose names do not end with 's'.
+SELECT *
+FROM accounts
+WHERE name NOT LIKE '%s';
+
+/*
+AND OPERATOR
+The AND operator is used within a WHERE statement to consider more than one logical clause at a time. Each time you link a new statement with an AND, you will need to specify the column you are interested in looking at. You may link as many statements as you would like to consider at the same time. 
+This operator works with all of the operations we have seen so far including arithmetic operators (+, *, -, /).
+LIKE, IN, and NOT logic can also be linked together using the AND operator.
+
+BETWEEN OPERATOR
+Sometimes we can make a cleaner statement using BETWEEN than we can using AND. Particularly this is true when we are using the same column for different parts of our AND statement.
+*/
+
+-- Write a query that returns all the orders where the standard_qty is over 1000, the poster_qty is 0, and the gloss_qty is 0
+SELECT *
+FROM orders
+WHERE standard_qty > 1000 AND poster_qty = 0 AND gloss_qty = 0;
+
+-- Using the accounts table, find all the companies whose names do not start with 'C' and end with 's'.
+SELECT *
+FROM accounts
+WHERE name NOT LIKE 'C%' AND name NOT LIKE '%s';
+
+-- When you use the BETWEEN operator in SQL, do the results include the values of your endpoints, or not? Figure out the answer to this important question by writing a query that displays the order date and gloss_qty data for all orders where gloss_qty is between 24 and 29.
+SELECT occurred_at, gloss_qty 
+FROM orders
+WHERE gloss_qty BETWEEN 24 AND 29;
+
+-- Use the web_events table to find all information regarding individuals who were contacted via the organic or adwords channels, and started their account at any point in 2016, sorted from newest to oldest.
+SELECT *
+FROM web_events
+WHERE channel IN ('organic', 'adwords') AND EXTRACT(YEAR FROM occurred_at)='2016'
+ORDER BY occurred_at DESC;
+
+SELECT *
+FROM web_events
+WHERE channel IN ('organic', 'adwords') AND occurred_at BETWEEN '2016-01-01' AND '2017-01-01'
+ORDER BY occurred_at DESC;
+
+/*
+OR OPERATOR
+Similar to the AND operator, the OR operator can combine multiple statements. Each time you link a new statement with an OR, you will need to specify the column you are interested in looking at. You may link as many statements as you would like to consider at the same time.
+This operator works with all of the operations we have seen so far including arithmetic operators (+, *, -, /), LIKE, IN, NOT, AND, and BETWEEN logic can all be linked together using the OR operator.
+
+When combining multiple of these operations, we frequently might need to use parentheses to assure that logic we want to perform is being executed correctly. 
+*/
+
+-- Find list of orders ids where either gloss_qty or poster_qty is greater than 4000. Only include the id field in the resulting table.
+SELECT id
+FROM orders
+WHERE gloss_qty > 4000 OR poster_qty > 4000;
+
+-- Write a query that returns a list of orders where the standard_qty is zero and either the gloss_qty or poster_qty is over 1000.
+SELECT *
+FROM orders
+WHERE standard_qty = 0 AND (poster_qty > 1000 OR gloss_qty>1000);
+
+-- Find all the company names that start with a 'C' or 'W', and the primary contact contains 'ana' or 'Ana', but it doesn't contain 'eana'.
+SELECT *
+FROM accounts
+WHERE (name LIKE 'C%' OR name LIKE'W%') AND (primary_poc LIKE '%ana%' OR primary_poc LIKE '%Ana%') AND primary_poc NOT LIKE '%eana%'; 
---
+++ postgrestutorial2(joins).sql
@@ -0,0 +1,221 @@
+-- 1. Try pulling all the data from the accounts table, and all the data from the orders table.
+SELECT accounts.*, orders.*
+FROM accounts
+JOIN orders
+ON orders.account_id=accounts.id
+LIMIT 20;
+-- Notice this result is the same as if you switched the tables in the FROM and JOIN. Additionally, which side of the = a column is listed doesn't matter.
+
+/*
+ The SELECT clause indicates which column(s) of data you'd like to see in the output (For Example, orders.* gives us all the columns in orders table in the output). The FROM clause indicates the first table from which we're pulling data, and the JOIN indicates the second table. 
+ The ON clause specifies the column on which you'd like to merge the two tables together.
+The ON statement holds the two columns that get linked across the two tables. This will be the focus in the next concepts.
+*/
+
+-- 2. Try pulling standard_qty, gloss_qty, and poster_qty from the orders table, and the website and the primary_poc from the accounts table.
+SELECT orders.standard_qty, orders.gloss_qty, 
+       orders.poster_qty,  accounts.website, 
+       accounts.primary_poc
+FROM orders
+JOIN accounts
+ON orders.account_id = accounts.id
+
+-- Query all the information from all the tables
+SELECT accounts.*, orders.*, region.*, sales_reps.*, web_events.*
+FROM accounts
+JOIN orders
+ON orders.account_id=accounts.id
+JOIN sales_reps
+ON accounts.sales_rep_id=sales_reps.id
+JOIN region
+ON sales_reps.region_id=region.id
+JOIN web_events
+ON web_events.account_id=accounts.id
+LIMIT 20;
+
+/*
+PRIMARY KEY: A primary key is a unique column in a particular table. This is the first column in each of our tables. Here, those columns are all called id, but that doesn't necessarily have to be the name.
+              It is common that the primary key is the first column in our tables in most databases.
+
+FOREIGN KEY: A foreign key is a column in one table that is a primary key in a different table.
+
+*/
+-----------------------------------------------------------
+-------------------JOIN WITH ALIAS-------------------------
+/*
+When we JOIN tables together, it is nice to give each table an alias. Frequently an alias is just the first letter of the table name.
+
+FROM tablename AS t1
+JOIN tablename2 AS t2
+
+*/
+
+/*
+Aliases for Columns in Resulting Table
+
+While aliasing tables is the most common use case. It can also be used to alias the columns selected to have the resulting table reflect a more readable name.
+
+Select t1.column1 aliasname, t2.column2 aliasname2
+FROM tablename AS t1
+JOIN tablename2 AS t2
+
+*/
+
+-- Provide a table for all web_events associated with account name of Walmart. There should be three columns. Be sure to include the primary_poc, time of the event, and the channel for each event. Additionally, you might choose to add a fourth column to assure only Walmart events were chosen.
+SELECT a.primary_poc, w.occurred_at, w.channel
+FROM accounts AS a
+JOIN web_events AS w
+ON a.id = w.account_id
+WHERE a.name='Walmart';
+
+-- Provide a table that provides the region for each sales_rep along with their associated accounts. Your final table should include three columns: the region name, the sales rep name, and the account name. Sort the accounts alphabetically (A-Z) according to account name.
+SELECT r.name AS region, s.name sales_representive, a.name AS account
+FROM accounts AS a
+JOIN sales_reps AS s
+ON a.sales_rep_id = s.id
+JOIN region AS r
+ON s.region_id = r.id
+ORDER BY a.name;
+/*
+We can alias a colomun with/without 'AS' keyword. If column names are same in different tables
+it's better to alias them in select clause otherwise they get mixed.
+In the above example if you don't alias name columns of individual columns the result is just
+one column.
+*/
+
+-- Provide the name for each region for every order, as well as the account name and the unit price they paid (total_amt_usd/total) for the order. Your final table should have 3 columns: region name, account name, and unit price. A few accounts have 0 for total,
+-- so I divided by (total + 0.01) to assure not dividing by zero.
+SELECT r.name AS region, a.name AS account, o.total_amt_usd/(o.total+0.01) AS unit_price
+FROM accounts AS a
+JOIN orders AS o
+ON a.id = o.account_id
+JOIN sales_reps AS s
+ON s.id = a.sales_rep_id
+JOIN region as r
+ON s.region_id = r.id;
+
+/*
+A LEFT JOIN and RIGHT JOIN do the same thing if we change the tables that are in the FROM and JOIN statements.
+
+A LEFT JOIN will at least return all the rows that are in an INNER JOIN.
+
+JOIN and INNER JOIN are the same.
+
+A LEFT OUTER JOIN is the same as LEFT JOIN
+*/
+
+-- Provide a table that provides the region for each sales_rep along with their associated accounts. This time only for the Midwest region. 
+-- Your final table should include three columns: the region name, the sales rep name, and the account name. Sort the accounts alphabetically (A-Z) according to account name.
+SELECT r.name region_name, s.name as sales_rep_name, a.name as account_name
+FROM sales_reps s
+JOIN accounts a
+ON a.sales_rep_id = s.id
+JOIN region r
+ON s.region_id = r.id
+WHERE r.name='Midwest'
+ORDER BY a.name;
+-- 48 results
+
+-- Provide a table that provides the region for each sales_rep along with their associated accounts. This time only for accounts where the sales rep has a first name starting with S and in the Midwest region. 
+-- Your final table should include three columns: the region name, the sales rep name, and the account name. Sort the accounts alphabetically (A-Z) according to account name.
+SELECT r.name region_name, s.name as sales_rep_name, a.name as account_name
+FROM sales_reps s
+JOIN accounts a
+ON a.sales_rep_id = s.id
+JOIN region r
+ON s.region_id = r.id
+WHERE r.name='Midwest' AND s.name LIKE 'S%'
+ORDER BY a.name;
+-- 5 results
+
+-- Provide a table that provides the region for each sales_rep along with their associated accounts. This time only for accounts where the sales rep has a last name starting with K and in the Midwest
+-- region. Your final table should include three columns: the region name, the sales rep name, and the account name. Sort the accounts alphabetically (A-Z) according to account name.
+SELECT r.name region_name, s.name as sales_rep_name, a.name as account_name
+FROM sales_reps s
+LEFT JOIN accounts a
+ON a.sales_rep_id = s.id
+LEFT JOIN region r
+ON s.region_id = r.id
+WHERE r.name='Midwest' AND s.name LIKE '%K%' AND STRPOS(s.name, 'K')!=1
+ORDER BY a.name;
+-- 13 results
+
+-- OR
+
+SELECT r.name region, s.name rep, a.name account
+FROM sales_reps s
+JOIN region r
+ON s.region_id = r.id
+JOIN accounts a
+ON a.sales_rep_id = s.id
+WHERE r.name = 'Midwest' AND s.name LIKE '% K%'
+ORDER BY a.name;
+
+-- Provide the name for each region for every order, as well as the account name and the unit price they paid (total_amt_usd/total) for the order. However, you should only provide the results if the 
+-- standard order quantity exceeds 100. Your final table should have 3 columns: region name, account name, and unit price. In order to avoid a division by zero error, adding .01 to the denominator
+-- here is helpful total_amt_usd/(total+0.01).
+LEFT JOIN accounts a
+ON a.id = o.account_id
+JOIN sales_reps as s
+ON s.id = a.sales_rep_id
+JOIN region r
+ON s.region_id = r.id
+WHERE o.standard_qty > 100
+ORDER BY a.name;
+-- 4509 results
+
+-- Provide the name for each region for every order, as well as the account name and the unit price they paid (total_amt_usd/total) for the order. However, you should only provide the results if the
+-- standard order quantity exceeds 100 and the poster order quantity exceeds 50. Your final table should have 3 columns: region name, account name, and unit price. Sort for the smallest unit price
+-- first. In order to avoid a division by zero error, adding .01 to the denominator here is helpful (total_amt_usd/(total+0.01).
+SELECT r.name region_name, a.name account_name,   o.total_amt_usd/(o.total+0.01) AS unit_price
+FROM orders as o
+LEFT JOIN accounts a
+ON a.id = o.account_id
+JOIN sales_reps as s
+ON s.id = a.sales_rep_id
+JOIN region r
+ON s.region_id = r.id
+WHERE o.standard_qty > 100 AND o.poster_qty > 50
+ORDER BY unit_price;
+-- 835 results
+
+-- Provide the name for each region for every order, as well as the account name and the unit price they paid (total_amt_usd/total) for the order. However, you should only provide the results if the
+-- standard order quantity exceeds 100 and the poster order quantity exceeds 50. Your final table should have 3 columns: region name, account name, and unit price. Sort for the largest unit price 
+-- first. In order to avoid a division by zero error, adding .01 to the denominator here is helpful (total_amt_usd/(total+0.01).
+SELECT r.name region_name, a.name account_name,   o.total_amt_usd/(o.total+0.01) AS unit_price
+FROM orders as o
+LEFT JOIN accounts a
+ON a.id = o.account_id
+JOIN sales_reps as s
+ON s.id = a.sales_rep_id
+JOIN region r
+ON s.region_id = r.id
+WHERE o.standard_qty > 100 AND o.poster_qty > 50
+ORDER BY unit_price DESC;
+-- 835 results
+
+-- What are the different channels used by account id 1001? Your final table should have only 2 columns: account name and the different channels. You can try SELECT DISTINCT to narrow down the 
+-- results to only the unique values.
+SELECT DISTINCT a.name, w.channel
+FROM accounts a
+JOIN web_events w
+ON a.id=w.account_id
+WHERE a.id=1001;
+-- 6 results
+
+-- Find all the orders that occurred in 2015. Your final table should have 4 columns: occurred_at, account name, order total, and order total_amt_usd.
+SELECT o.occurred_at, a.name, o.total, o.total_amt_usd 
+FROM orders o
+JOIN accounts a
+ON a.id=o.account_id
+WHERE EXTRACT(YEAR FROM o.occurred_at) = '2015';
+-- 1725 results
+
+-- OR
+
+SELECT o.occurred_at, a.name, o.total, o.total_amt_usd
+FROM accounts a
+JOIN orders o
+ON o.account_id = a.id
+WHERE o.occurred_at BETWEEN '01-01-2015' AND '01-01-2016'
+ORDER BY o.occurred_at DESC;
